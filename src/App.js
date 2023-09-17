29ba0e785a0c6d9db0cc95530842546657f78528
ShambhavII-Singh <shambhavi.singh2021@vitstudent.ac.in>
2023-09-17 23:49:56 +0530

    created loading screen 4

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import './App.css';
 import LoadingScreen from './containers/loadingScreen';
+import { AnimatePresence } from 'framer-motion';
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <p>hahahah </p>
-        </header>
-      </div>
+      
+        <div className="App">
+          <header className="App-header">
+          <AnimatePresence>
+            <p>hahahah </p>
+          </AnimatePresence>
+          </header>
+        </div>
+      
     );
   }
 }
--- src/containers/loadingScreen.jsx
+++ src/containers/loadingScreen.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { Fade } from "react-bootstrap";
 import Lottie from "lottie-react";
 import loadingAnimationDark from '../assets/loadingAnimationDark.json'
 import loadingAnimationLight from '../assets/loadingAnimationLight.json'
+import { AnimatePresence } from "framer-motion";
 
 const LoadingScreen = (WrappedComponent) => {
+
    return class extends React.Component {
       constructor(props){
          super(props)
@@ -14,30 +15,34 @@ const LoadingScreen = (WrappedComponent) => {
          }
       }
 
-      // async componentDidMount() {
-      //    try {
-      //       setTimeout(() => {
-      //          this.setState({
-      //             done: true,
-      //          });
-      //       }, 1500)
-      //    } catch (err) {
-      //       console.log(err);
-      //       this.setState({
-      //          done: true,
-      //       });
-      //    }
-      // }
+      async componentDidMount() {
+         try {
+            setTimeout(() => {
+               this.setState({
+                  done: true,
+               });
+            }, 3000)
+         } catch (err) {
+            console.log(err);
+            this.setState({
+               done: true,
+            });
+         }
+      }
 
       render() {
          return(
-            <div>
-               {!this.state.done ? (
-                  this.state.darkTheme ? <Lottie animationData={loadingAnimationDark} loop={true} width={"200px"} height={200} /> : <Lottie animationData={loadingAnimationLight} loop={true} width={200} height={200}/>
-               ) : (
-                  <Fade><WrappedComponent {...this.props} /></Fade>
-               )}
-            </div>
+            
+               <div>
+                  {!this.state.done ? (
+                     this.state.darkTheme ? 
+                        <Lottie animationData={loadingAnimationDark} loop={true} /> : 
+                        <Lottie animationData={loadingAnimationLight} loop={true} />
+                  ) : (
+                     <AnimatePresence><WrappedComponent {...this.props} /></AnimatePresence>
+                  )}
+               </div>
+            
          )
       }
    }
