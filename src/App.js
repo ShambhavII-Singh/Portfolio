ee70d1360316ed20221ea454e2636d9be8c48b5f
ShambhavII-Singh <shambhavi.singh2021@vitstudent.ac.in>
2023-09-17 14:56:30 +0530

    created loading screen 1

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Loading from '././components/loading';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>hahahah </p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default Loading(App);
---
+++ src/components/loading.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import ReactLoading from "react-loading";
+import { Fade } from "react-bootstrap";
+
+const Loading = (WrappedComponent) => {
+   return class extends React.Component {
+      constructor(props){
+         super(props)
+         this.state = {
+            done: undefined
+         }
+      }
+
+      // async componentDidMount() {
+      //    try {
+      //       setTimeout(() => {
+      //          this.setState({
+      //             done: true,
+      //          });
+      //       }, 1500)
+      //    } catch (err) {
+      //       console.log(err);
+      //       this.setState({
+      //          done: true,
+      //       });
+      //    }
+      // }
+
+      render() {
+         return(
+            <div>
+               {!this.state.done ? (
+                  <ReactLoading type={"bars"} color={"red"} />
+               ) : (
+                  <Fade><WrappedComponent {...this.props} /></Fade>
+               )}
+            </div>
+         )
+      }
+   }
+}
+
+export default Loading;
+
